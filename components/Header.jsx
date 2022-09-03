f53b21d8b3cc2cb9b2c0622971e38212ebb94ca1
olaoyetijani <adeolatijani12@gmail.com>
2022-09-03 11:53:58 +0100

    next js blog

---
+++ components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { getCategories } from '../services'
+
+
+const Header = () => {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories().then((newCategories) => setCategories(newCategories))
+    }, [])
+
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+        <div className="border-b w-full inline-block border-white-400 py-8">
+            <div className="md:float-left block">
+                <Link href="/">
+                    <span className="cursor-pointer font-bold text-4xl text-white">
+                        TechMunch
+                    </span>
+                </Link>
+            </div>
+            <div className="hidden md:float-left md:contents">
+                {categories.map((category) => (
+                    <Link key={category.slug} href={`/category/${category.slug}`}>
+                        <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                            {category.name}
+                        </span>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ components/PostWidget.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import { getRecentPosts, getSimilarPosts } from '../services'
+
+function PostWidget({ categories, slug }) {
+  const [relatedPosts, setRelatedPosts] = useState([])
+
+  useEffect(() => {
+    if(slug) {
+      getSimilarPosts(categories, slug).then((result) => setRelatedPosts(result))
+    }
+    else {
+      getRecentPosts().then((result) => setRelatedPosts(result))
+    }
+  }, [slug])
+  
+  return (
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+      <h1 className='text-xl mb-8 font-semibold border-b pb-4'>{slug ? "Related Post" : "Recent Posts"}</h1>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img src={post.featuredImage.url} alt={post.title} height='60px' width='60px' />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className='text-gray-500 font-xs'>{moment(post.createdAt).format('MMM DD, YYYY')}</p>
+            <Link href={`/post/${post.slug}`} key={post.title} className='text-md'>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default PostWidget
